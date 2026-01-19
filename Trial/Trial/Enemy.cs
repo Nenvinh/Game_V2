4c03e97c0dcf1c0ba74aa4ee88e7cb5f74f1e5da
Nenvinh <135128068+Nenvinh@users.noreply.github.com>
2026-01-19 23:04:47 +0100

    Add files via upload
    Přidal jsem: 
    1. Dodělaný konstuktory všech tříd
    2. Z většiny hotovou třídu Player
    3. Zadávání jména hráče

--- Trial/Trial/Enemy.cs
+++ Trial/Trial/Enemy.cs
@@ -1,6 +1,15 @@
-﻿namespace Trial;
+namespace Trial;
 
-public class Enemy
+public class Enemy : Entity
 {
-    
+    public Enemy(string name, int damage, int max_health, int current_health, int armor, int heal) :
+        base(name, damage, max_health, current_health, armor, heal)
+    {
+        this.Name = name;
+        this.Damage = damage;
+        this.Max_Health = max_health;
+        this.Current_Health = current_health;
+        this.Armor = armor;
+        this.Heal = heal;
+    }
 }
--- Trial/Trial/Program.cs
+++ Trial/Trial/Program.cs
@@ -1,29 +1,103 @@
-﻿void intro()
+﻿namespace Trial;
+using System;
+
+public class Program
 {
-    Console.WriteLine("███████         █████         ██████         ███████");
-    System.Threading.Thread.Sleep(1000);
-    Console.WriteLine("██             ██   ██        ██   ██        ██     ");
-    System.Threading.Thread.Sleep(1000);
-    Console.WriteLine("█████          ███████        ██████         ███████");
-    System.Threading.Thread.Sleep(1000);
-    Console.WriteLine("██             ██   ██        ██   ██             ██");
-    System.Threading.Thread.Sleep(1000);
-    Console.WriteLine("███████ ██     ██   ██ ██     ██   ██ ██     ███████ ██");
-    System.Threading.Thread.Sleep(1000);
-    Console.WriteLine("Electronic action recording software");
-    Console.WriteLine("Developed by Miliware");
-    Console.WriteLine("V 0.86");
-    Console.WriteLine("Press any key to exit stand by mode...");
-    Console.ReadKey();
-    Console.Clear();
-    Console. WriteLine("Pokracovani");
+    static void intro()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("███████         █████         ██████         ███████");
+        System.Threading.Thread.Sleep(1000);
+        Console.WriteLine("██             ██   ██        ██   ██        ██     ");
+        System.Threading.Thread.Sleep(1000);
+        Console.WriteLine("█████          ███████        ██████         ███████");
+        System.Threading.Thread.Sleep(1000);
+        Console.WriteLine("██             ██   ██        ██   ██             ██");
+        System.Threading.Thread.Sleep(1000);
+        Console.WriteLine("███████ ██     ██   ██ ██     ██   ██ ██     ███████ ██");
+        System.Threading.Thread.Sleep(1000);
+        Console.WriteLine("Electronic action recording software");
+        Console.WriteLine("Developed by Miliware");
+        Console.WriteLine("V 0.86");
+        Console.WriteLine("Press any key to exit stand by mode...");
+        Console.ReadKey();
+        Console.Clear();
+        Console. WriteLine("Nový uživatel detekován");
+        Console. WriteLine("Příprava nové instance....");
+        System.Threading.Thread.Sleep(1000);
+        Console.WriteLine("Nahrávání programů.....");
+        System.Threading.Thread.Sleep(1000);
+        Console.WriteLine("Zjištování ůdajů o uživateli.....");
+        Console.Clear();
+    }
+
+    static int vyber_tridy(string jmeno)
+    {
+        Console.Write($"Vítejte {jmeno} ");
+        Console.WriteLine("Zadejte vaše zaměření: ");
+        Console.WriteLine("1. RIFLEMAN (začíná se armorem)");
+        Console.WriteLine("2. COMMANDER  (začíná se více penězi)");
+        Console.WriteLine("3. SPECIAL FORCES (začíná se více HP)");
+        Console.WriteLine("4. MEDIC (léčí se více HP)");
+        Console.Write("Zadejte: ");
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.ForegroundColor = ConsoleColor.Green;
+                break;
+            case 2:
+                Console.ForegroundColor = ConsoleColor.Red;
+                break;
+            case 3:
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                break;
+            case 4:
+                Console.ForegroundColor = ConsoleColor.Green;
+                break;
+            default:
+                Console.WriteLine("Neznámý vztup");
+                break;
+                    
+        }
+        
+        
+        return 0;
+    }
+
+    static string zadani_jmena()
+    {
+        string jmeno;
+        while (true)
+        {
+            Console.WriteLine("Vitejte");
+            Console.Write("Zadjete vase jmeno (bez mezer): ");
+            jmeno = Console.ReadLine();
+            Console.Write("Jste jsi jistý? (ano/ne): ");
+            string confirmation = Console.ReadLine();
+            Console.Clear();
+            if (confirmation == "ano")
+            {
+                break;
+            }
+        }
+        return jmeno;
+    }
+    
+    static int Main()
+    {
+        intro();
+        zadani_jmena();
+        vyber_tridy(zadani_jmena());
+        
+
+        
+        
+        
+        
+        
+        return 0;
+    }
 }
 
-
-
-int main()
-{ 
-    intro();
-    return 0;
-}
 

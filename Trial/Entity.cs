c40d8657a07f05558e46a10f0af3f1fdbadef4cf
Nenvinh <135128068+Nenvinh@users.noreply.github.com>
2026-02-08 15:06:40 +0100

    Add files via upload
    Přidáno:
    Polymorfická funkce Attack. Funguje na principu <iteration>.Attack(target)

---
+++ Trial/Entity.cs
@@ -0,0 +1,213 @@
+namespace Trial;
+
+/*
+_name <---- malé písmeno na začátku = privátní člen mother class
+Name <---- velké písmeno na začátku = public člen mother class
+Damage_ <- velké písmeno na začátku + podtržítko na konci = Polymorphism
+
+*/
+
+public class Entity
+{
+    private string name;
+    private int damage;
+    private int max_health;
+    private int current_health;
+    private int armor;
+    private int heal;
+    private int money;
+    public bool is_alive = true;
+
+
+    public string Name
+    {
+        get{return name;}
+        set{
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Jméno nemůže být prázdné");
+            }
+            if (value.Length > 50)
+            {
+                throw new ArgumentException("Jméno je příliš dlouhý (max 50 znaků).");
+            }
+            else
+            {
+                name = value;
+            }
+            
+        }
+    }
+
+    public int Damage
+    {
+        get{return damage;}
+        set
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Damage nemůže být méně než 0.");
+            }
+            else
+            {
+                damage = value;
+            }
+        }
+    }
+
+    public int Max_Health
+    {
+        get{return max_health;}
+        set
+        {
+            if (value <= 0)
+            {
+                Console.WriteLine("Max Health nemůže být 0 a némě.");
+            }
+            else
+            {
+                max_health = value;
+            }
+        }
+    }
+
+    public int Current_Health
+    {
+        get{return current_health;}
+        set
+        {
+            if (value <= 0)
+            {
+                Console.WriteLine("Současné životy je 0 a méně.");
+            }
+            else
+            {
+                current_health = value;
+            }
+        }
+    }
+
+    public int Armor
+    {
+        get{return armor;}
+        set
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Armor nemůže být 0 a némě.");
+            }
+            else
+            {
+                armor = value;
+            }
+        }
+    }
+
+    public int Heal
+    {
+        get{return heal;}
+        set
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Heal nemůže být méně než 0.");
+            }
+            else
+            {
+                heal = value;
+            }
+        }
+    }
+
+    public int Money
+    {
+        get { return money; }
+        set { money = value; }
+    }
+    
+    
+    public int Heal_(int heal, int current_health, int max_health)
+    {
+        current_health = current_health + heal;
+        if (current_health > max_health)
+        {
+            current_health = max_health;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"Overheal na max health {max_health} hp.");
+            Console.ResetColor();
+            return current_health;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Healing +{heal} hp.");
+            Console.ResetColor();
+            return current_health;
+        }
+    }
+
+
+    virtual public string Get_info()
+    {
+        return $"Jméno: {name} | Útok: {damage} | Max životy: {max_health} | Souč. životy: {current_health} | Ochrana (armor): {armor} | Heal: {heal} | Money: {money} |";
+    }
+    
+    //============================================ATTACK & UBÍRÁNÍ ŽIVOTA============================================
+    
+    public virtual int CalculateAttackDamage()
+    {
+        return Damage;
+    }
+
+    public virtual void TakeDamage(int damage)
+    {
+        int reduced = damage - Armor;
+        if (reduced < 0)
+        {
+            reduced = 0;
+        }
+        current_health -= reduced;
+    }
+    
+    public virtual void Attack(Entity target)
+    {
+        int finalDamage = CalculateAttackDamage();
+        target.TakeDamage(finalDamage);
+    }
+    
+    //============================================ATTACK & UBÍRÁNÍ ŽIVOTA============================================
+
+    public bool Life_monitoring(int status)
+    {
+        if (status <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    
+    
+    
+    //konstuktor
+    public Entity(string name, int damage, int max_health, int current_health, int armor, int heal, int money)
+    {
+        this.Name = name;
+        this.Damage = damage;
+        this.Max_Health = max_health;
+        this.Current_Health = current_health;
+        this.Armor = armor;
+        this.Heal = heal;
+        this.Money = money;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ Trial/Program.cs
@@ -0,0 +1,219 @@
+﻿namespace Trial;
+using System;
+
+public class Program
+{
+    static void intro()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("███████         █████         ██████         ███████");
+        Thread.Sleep(1000);
+        Console.WriteLine("██             ██   ██        ██   ██        ██     ");
+        Thread.Sleep(1000);
+        Console.WriteLine("█████          ███████        ██████         ███████");
+        Thread.Sleep(1000);
+        Console.WriteLine("██             ██   ██        ██   ██             ██");
+        Thread.Sleep(1000);
+        Console.WriteLine("███████ ██     ██   ██ ██     ██   ██ ██     ███████ ██");
+        Thread.Sleep(1000);
+        Console.WriteLine("Electronic action recording software");
+        Console.WriteLine("Developed by Miliware");
+        Console.WriteLine("V 0.86");
+        Console.Write("Press any key to exit stand by mode...");
+        Console.ReadKey();
+        Console.Clear();
+        Console. WriteLine("Nový uživatel detekován");
+        Console. WriteLine("Příprava nové instance....");
+        Thread.Sleep(1000);
+        Console.WriteLine("Nahrávání programů.....");
+        Thread.Sleep(1000);
+        Console.WriteLine("Zjištování ůdajů o uživateli.....");
+        Console.ResetColor();
+        Console.Clear();
+    }
+
+    static string vyber_tridy()
+    {
+        
+        
+        while (true)
+        {
+            Console.WriteLine("zadejte vaše zaměření: ");
+            Console.WriteLine("1. RIFLEMAN (začíná se armorem)");
+            Console.WriteLine("2. COMMANDER  (začíná se více penězi)");
+            Console.WriteLine("3. SPECIAL FORCES (útoky berou více HP)");
+            Console.WriteLine("4. MEDIC (léčí se více HP)");
+            Console.Write("Zadejte: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Vybráno: RIFLEMAN");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    return "rifleman";
+                    break;
+                case 2:
+                    Console.WriteLine("Vybráno: COMMANDER");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    return "commander";
+                    break;
+                case 3:
+                    Console.WriteLine("Vybráno: SPECIAL FORCES");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    return "special";
+                    break;
+                case 4:
+                    Console.WriteLine("Vybráno: MEDIC");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    return "medic";
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Neznámý vztup");
+                    Thread.Sleep(1000);
+                    Console.ResetColor();
+                    Console.Clear();
+                    break;
+                    
+            }
+        
+        
+            
+        }
+    }
+
+    static string zadani_jmena()
+    {
+        string jmeno;
+        while (true)
+        {
+            Console.WriteLine("Vitejte");
+            Console.Write("Zadjete vase jmeno (bez mezer): ");
+            jmeno = Console.ReadLine();
+            Console.Write("Jste jsi jistý? (ano/ne): ");
+            string confirmation = Console.ReadLine();
+            Console.Clear();
+            if (confirmation == "ano")
+            {
+                break;
+            }
+        }
+        Console.Write($"Vítejte {jmeno}, ");
+        return jmeno;
+    }
+    
+    static int Main()
+    {
+        //intro();
+        
+        Player hrac = new Player("Random", 1, 1, 1, 1, 1, 1, 30, "Random", " ", '0', '0', '0', '0', '0');
+        hrac.Name = zadani_jmena();
+        hrac.Trida = vyber_tridy();
+        
+        //switch na zadávání dat tříd
+        switch (hrac.Trida)
+        {
+            case "rifleman":
+                hrac.Damage = 10;
+                hrac.Damage_upgrade = '0';
+                hrac.Max_Health = 20;
+                hrac.Max_health_upgrade = '0';
+                hrac.Current_Health = hrac.Max_Health;
+                hrac.Armor = 10;
+                hrac.Armor_upgrade = '1';
+                hrac.Heal = 5;
+                hrac.Heal_upgrade = '0';
+                hrac.Money = 10;
+                hrac.Ammo = 30;
+                //hrac.Ammo_upgrade = '0';
+                break;
+            case "commander":
+                hrac.Damage = 10;
+                hrac.Damage_upgrade = '0';
+                hrac.Max_Health = 20;
+                hrac.Max_health_upgrade = '0';
+                hrac.Current_Health = hrac.Max_Health;
+                hrac.Armor = 0;
+                hrac.Armor_upgrade = '0';
+                hrac.Heal = 5;
+                hrac.Heal_upgrade = '0';
+                hrac.Money = 30;
+                hrac.Ammo = 30;
+                //hrac.Ammo_upgrade = '0';
+                break;
+            case "medic":
+                hrac.Damage = 10;
+                hrac.Damage_upgrade = '0';
+                hrac.Max_Health = 20;
+                hrac.Max_health_upgrade = '0';
+                hrac.Current_Health = 20;
+                hrac.Armor = 0;
+                hrac.Armor_upgrade = '0';
+                hrac.Heal = 10;
+                hrac.Heal_upgrade = '1';
+                hrac.Money = 10;
+                hrac.Ammo = 30;
+                //hrac.Ammo_upgrade = '0';
+                break;
+            case "special":
+                hrac.Damage = 20;
+                hrac.Damage_upgrade = '1';
+                hrac.Max_Health = 20;
+                hrac.Max_health_upgrade = '0';
+                hrac.Current_Health = hrac.Max_Health;
+                hrac.Armor = 10;
+                hrac.Armor_upgrade = '0';
+                hrac.Heal = 5;
+                hrac.Heal_upgrade = '0';
+                hrac.Money = 10;
+                hrac.Ammo = 30;
+                //hrac.Ammo_upgrade = '0';
+                break;
+        }
+        
+        /*
+        Console.WriteLine(hrac.Get_info());
+        hrac.Current_Health = hrac.Damage_(20, hrac.Current_Health, hrac.Armor);
+        Console.WriteLine($"Damage: {hrac.Damage_(20, hrac.Current_Health, hrac.Armor)}");
+        Console.WriteLine(hrac.Get_info());
+        hrac.Current_Health = hrac.Heal_(hrac.Heal, hrac.Current_Health, hrac.Max_Health);
+        Console.WriteLine($"Heal: {hrac.Heal_(hrac.Heal, hrac.Current_Health, hrac.Max_Health)}");
+        Console.WriteLine(hrac.Get_info());
+        */
+        
+        Console.Clear();
+        Enemy random = new Enemy("Shotgun", 5, 10, 10, 5, 0, 5);
+        Console.WriteLine(hrac.Get_info());
+        Console.WriteLine(random.Get_info());
+        
+        hrac.Attack(random);
+        
+        Console.WriteLine(hrac.Get_info());
+        Console.WriteLine(random.Get_info());
+
+        
+        
+        /*while (hrac.is_alive)
+        {
+          
+          hrac.is_alive = hrac.Life_monitoring(hrac.Current_Health); 
+        
+        
+        
+        
+        
+        
+        }
+        */
+        
+        
+        
+        return 0;
+    }
+}
+
+
